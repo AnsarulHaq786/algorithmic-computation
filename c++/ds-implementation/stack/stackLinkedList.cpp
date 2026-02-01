42c674272a68076b490eb871e8b71d47eda7d953
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2026-02-01 23:28:40 +0545

    ds: implementaton of stack using linked list

--- c++/ds-implementation/stack/stackLinkedList.cpp
+++ c++/ds-implementation/stack/stackLinkedList.cpp
@@ -26,13 +26,42 @@ class Stack{
     }
     //pop
     int pop(){
-        
-    }
-    //topElememt
-    //isEmpty
-    //isFull
+        if(isEmpty()) {
+            cout<<"stack is empty";
+            return 0;
+        }
+        else {
+            int item=head->data;
+            Node* temp = head;
+            head=head->next;
+            delete temp;
+            return item; 
+        }
     
+    //empty
+    }
+    bool isEmpty() {
+        if(head==nullptr) return true;
+        else return false;
+    }
+    
+    void display() {
+        if (isEmpty()) {
+            cout << "Stack is empty" << endl;
+            return;
+        }
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << endl;
+            temp=temp->next;
+        }
+    }
 };
 int main(){
-    
+    Stack s;
+    s.push(5);
+    s.push(3);
+    s.push(6);
+    s.display();
+    return 0;
 }
