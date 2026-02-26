dd3aff25e9b4a797cf6f014d68b37428352b91bd
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2026-02-26 06:08:14 +0545

    sort: bubble sort descending order

---
+++ c++/algorithms/sorting-algorithms/bubbleSort.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void bubbleSort(vector<int> &arr) {
+    int n=arr.size();
+    for(int i=0; i<n-1; i++) {
+        for(int j=0; j<n-i-1; j++) {
+            if(arr[j]<arr[j+1]) swap(arr[j], arr[j+1]);
+        }
+    }
+}
+
+int main() {
+    vector<int> arr={4, 5, 3, 1, 2};
+    bubbleSort(arr);
+    for(int i=0; i<5; i++) {
+        cout<<arr[i]<<" ";
+    }
+}
