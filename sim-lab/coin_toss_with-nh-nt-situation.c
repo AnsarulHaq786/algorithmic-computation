6c1c470db4b39418e55ac3014a81e62b53c7667e
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2026-05-19 08:16:00 +0545

    coin toss simulation with no head and no tail situation

---
+++ sim-lab/coin_toss_with-nh-nt-situation.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+int main() {
+    int n, i, head_count=0, tail_count=0, no_head_tail_count=0, toss;
+    printf("Enter number of toses:");
+    scanf("%d", &n);
+    srand(time(0));
+    for(i=0; i<n; i++) {
+        toss=rand()%100;
+        printf("%d ", toss);
+        if(toss<49) head_count++;
+        else if(toss<98) tail_count++;
+        else no_head_tail_count++;
+    }
+    float prob_head=(float)head_count/n, prob_tail=(float)tail_count/n, prob_nht=(float)no_head_tail_count/n;
+    printf("\nheads: %d, probability of head: %.2f\n", head_count, prob_head);
+    printf("tails: %d, probability of tail: %.2f\n", tail_count, prob_tail);
+    printf("no head tail: %d, probability of tail: %.2f", no_head_tail_count, prob_nht);
+    return 0;
+}
